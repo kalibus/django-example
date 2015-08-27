13bc72c7bef458f714643b6735f82ce6f5dcea74
Michael Mathieu <nemesis@MacBook-Pro-de-Michael.local>
2015-08-26 22:51:53 -0300

    First Upload

---
+++ polls/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.utils import timezone
+import datetime
+
+
+class Question(models.Model):
+    #Pregunta
+    question_text = models.CharField(max_length=200)
+    #Fecha de Pub
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+
+class Choice(models.Model):
+    #Question asociada mediante ForeignKey
+    question = models.ForeignKey(Question)
+    #Texto de la opcion
+    choice_text = models.CharField(max_length=200)
+    #Contador de votos
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+# Create your models here.
